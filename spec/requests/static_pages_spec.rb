1fd21c9672814ee9617bbc1f978a6e2af7df91f2
Zachary Palchick <zpalchick@5amsolutions.com>
2013-11-21 21:19:15 -0500

    Added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"} 
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -10,7 +10,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
         visit '/static_pages/home'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+        expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -21,7 +21,7 @@ describe "Static pages" do
     end
     it "should have the title 'Help'" do
         visit '/static_pages/help'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+        expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -32,7 +32,18 @@ describe "Static pages" do
     end
     it "should have the title 'About Us'" do
         visit '/static_pages/about'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+        expect(page).to have_title("#{base_title} | About Us")
     end
+  end
+
+  describe "Content Page" do
+      it "should have the content 'Contact Us'" do
+          visit '/static_pages/contact'
+          expect(page).to have_content('Contact Us')
+      end
+      it "should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+      end
   end
 end

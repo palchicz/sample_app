d2c173a15765781075aeac0b985ba547faa49326
Zachary Palchick <zpalchick@5amsolutions.com>
2014-01-11 17:46:58 -0500

    completed exerciese for ch 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,25 +13,38 @@ describe "User Pages" do
                 expect { click_button submit }.not_to change(User, :count)
             end
             
-            describe "after submissiom" do
+            describe "after submission" do
                 before { click_button submit }
+
                 it { should have_title('Sign up') }
                 it { should have_content('error') }
+                it { should have_content('Name can\'t be blank') }
+                it { should have_content('Email can\'t be blank') }
                 it { should have_content('Email is invalid') } 
+                it { should have_content('Password can\'t be blank') }
                 it { should have_content('Password is too short') }
             end
         end
 
         describe "with valid information" do
+            let(:exampleEmail) { "user@example.com" }
             before do
                 fill_in "Name",     with: "Example User"
-                fill_in "Email",    with: "user@example.com"
+                fill_in "Email",    with:  exampleEmail
                 fill_in "Password", with: "foobar"
                 fill_in "Password confirmation", with: "foobar"
             end
             
             it "should create a user" do
                 expect { click_button submit }.to change(User, :count)
+            end
+
+            describe "after submission" do
+                before { click_button submit }
+                let(:user) { User.find_by(email: exampleEmail) }
+
+                it { should have_title(user.name) }
+                it { should have_selector('div.alert.alert-success', text: 'Welcome') }
             end
         end
         it { should have_content('Sign up') }
@@ -45,5 +58,6 @@ describe "User Pages" do
 
         it { should have_content(user.name) }
         it { should have_title(user.name) }
+        it { should have_selector('img.gravatar') }
     end
 end

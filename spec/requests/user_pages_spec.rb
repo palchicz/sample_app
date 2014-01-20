581ef6b96db67a535589f4b9e66541aef6e41d99
Zachary Palchick <zpalchick@5amsolutions.com>
2014-01-19 23:25:32 -0500

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,55 +1,142 @@
 require 'spec_helper'
 
 describe "User Pages" do
-  subject { page }
+    subject { page }
 
-  describe "signup page" do
-    before { visit signup_path }
+    describe "index" do
+        let(:user) { FactoryGirl.create(:user) }
+        before(:each) do
+            sign_in user
+            visit users_path
+        end
+     
+        it { should have_title('All users') }
+        it { should have_content('All users') }
+        
+        describe "pagination" do
+            before(:all) { 30.times { FactoryGirl.create(:user) } }
+            after(:all) { User.delete_all }
 
+            it { should have_selector('div.pagination') }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { invalid_signup }.not_to change(User, :count)
-      end
+            it "should list each user" do
+                User.paginate(page: 1).each do |user|
+                    expect(page).to have_selector('li', text: user.name)
+                end
+            end
+        end
 
-      describe "after submission" do
-        before { invalid_signup }
+        describe "delete links" do
+            it { should_not have_link('delete') }
 
-        it { should have_title('Sign up') }
-        it { should have_content('error') }
-        it { should have_content('Name can\'t be blank') }
-        it { should have_content('Email can\'t be blank') }
-        it { should have_content('Email is invalid') } 
-        it { should have_content('Password can\'t be blank') }
-        it { should have_content('Password is too short') }
-      end
+            describe "as an admin user" do
+                let(:admin) { FactoryGirl.create(:admin) }
+                before do
+                    sign_in admin
+                    visit users_path
+                end
+
+                it { should have_link('delete', href: user_path(User.first)) }
+                it { should_not have_link('delete', href: user_path(admin)) }
+                it "should be able to delete another user" do
+                    expect do
+                        click_link('delete', match: :first)
+                    end.to change(User, :count).by(-1)
+                end
+
+                describe "after deletion" do
+                    before { click_link('delete', match: :first) }
+                    it { should have_success_message }
+                end
+            end
+        end
     end
 
-    describe "with valid information" do
-      it "should create a user" do
-        expect { valid_signup }.to change(User, :count)
-      end
+    describe "edit" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+            sign_in user
+            visit edit_user_path(user) 
+        end
 
-      describe "after submission" do
-        before { valid_signup }
-        let(:user) { User.find_by(email: SignupHelper::VALID_EMAIL) }
+        describe "page" do
+            it { should have_content("Update your profile") }
+            it { should have_title("Edit user") }
+            it { should have_link('change', href: 'http://gravatar.com/emails') }
+        end
 
-        it { should have_link('Sign out') }
+        describe "with invalid information" do
+            before { click_button "Save changes" }
+
+            it { should have_content('error') }
+        end
+
+        describe "with valid information" do
+            let(:new_name) { "New Name" }
+            let(:new_email) { "new@example.com" }
+            before do
+                fill_in "Name",         with: new_name
+                fill_in "Email",        with: new_email
+                fill_in "Password",     with: user.password
+                fill_in "Confirm Password", with: user.password
+                click_button "Save changes"
+            end
+            it { should have_title(new_name) }
+            it { should have_success_message }
+            it { should have_link('Sign out', href: signout_path) }
+            specify { expect(user.reload.name).to eq new_name }
+            specify { expect(user.reload.email).to eq new_email }
+        end
+    end
+
+
+    describe "signup page" do
+        before { visit signup_path }
+
+
+        describe "with invalid information" do
+            it "should not create a user" do
+                expect { invalid_signup }.not_to change(User, :count)
+            end
+
+            describe "after submission" do
+                before { invalid_signup }
+
+                it { should have_title('Sign up') }
+                it { should have_content('error') }
+                it { should have_content('Name can\'t be blank') }
+                it { should have_content('Email can\'t be blank') }
+                it { should have_content('Email is invalid') } 
+                it { should have_content('Password can\'t be blank') }
+                it { should have_content('Password is too short') }
+            end
+        end
+
+        describe "with valid information" do
+            it "should create a user" do
+                expect { valid_signup }.to change(User, :count)
+            end
+
+            describe "after submission" do
+                before { valid_signup }
+                let(:user) { User.find_by(email: SignupHelper::VALID_EMAIL) }
+
+                it { should have_link('Sign out') }
+                it { should have_title(user.name) }
+                it { should have_success_message('Welcome') }
+            end
+        end
+        it { should have_content('Sign up') }
+        it { should have_title(full_title('Sign up')) }
+    end
+
+    describe "profile page" do
+        let(:user) { FactoryGirl.create(:user) }
+
+        before { visit user_path(user) }
+
+        it { should have_content(user.name) }
         it { should have_title(user.name) }
-        it { should have_success_message('Welcome') }
-      end
+        it { should have_selector('img.gravatar') }
     end
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
-
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-
-    before { visit user_path(user) }
-
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-    it { should have_selector('img.gravatar') }
-  end
 end
